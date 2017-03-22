96bf397a0d3673235163db88fb1cb4037179f750
minji <mjna231@gmail.com>
2017-03-22 09:43:35 +0900

    array string, integer

--- ruby/lotto_crawling.rb
+++ ruby/lotto_crawling.rb
@@ -3,27 +3,20 @@ require 'open-uri'
 
 
 numbers = Array.new
+sample = Array.new
 doc = Nokogiri::HTML(open("https://search.naver.com/search.naver?where=nexearch&query=%EB%A1%9C%EB%98%90%EB%8B%B9%EC%B2%A8%EB%B2%88%ED%98%B8&sm=top_sug.mbk&fbm=1&acr=1&acq=%EB%A1%9C%EB%98%90%EB%8B%B9%EC%A0%90&qdt=0&ie=utf8", 'User-Agent' => 'ruby'))
 lotto_number = doc.css(".num_box .num")
 
-puts '<numbers>'
 lotto_number.each do |n|
-  numbers << n.inner_text
+  numbers << n.inner_text.to_i
 end
+puts '<numbers>'
 puts numbers
 
-puts '<sample>'
-number_list = Array 1..45
+number_list = Array 1..45  #Array '1'..'45'
 sample = number_list.sample(7)
+puts '<sample>'
 puts sample
-
-# @numbers = [3, 25, 12, 36, 9, 10, 11]
-# puts @numbers
-# puts ""
-
-# @sample = [25, 36, 12, 4, 5, 6, 7]
-# puts '<sample>'
-# puts @sample\
 
 match = Array.new
 
@@ -34,6 +27,7 @@ match = Array.new
 end
 
 puts ''
-puts '<일치갯수>'
+puts '<일치하는 번호>'
 puts match
+puts '<일치갯수>'
 puts match.size
